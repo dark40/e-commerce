0556a8a9e31cb984c9141d2a804963e925f1bc3a
Freddie Yang <33718503+dark40@users.noreply.github.com>
2024-04-19 16:47:08 +1000

    added categories

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -19,6 +19,7 @@ import { generateMeta } from '../../_utilities/generateMeta'
 // If you are not using Payload Cloud then this line can be removed, see `../../../README.md#cache`
 export const dynamic = 'force-dynamic'
 
+import Categories from '../../_components/Categories'
 import { Gutter } from '../../_components/Gutter'
 
 import classes from './index.module.scss'
@@ -62,7 +63,9 @@ export default async function Page({ params: { slug = 'home' } }) {
       {slug === 'home' ? (
         <section>
           <Hero {...hero} />
-          <Gutter>{/* categories */}</Gutter>
+          <Gutter className={classes.home}>
+            <Categories categories={categories} />
+          </Gutter>
         </section>
       ) : (
         <>
--- src/app/_api/fetchDoc.ts
+++ src/app/_api/fetchDoc.ts
@@ -1,6 +1,7 @@
 import type { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies'
 
 import type { Config } from '../../payload/payload-types'
+import { CATEGORIES } from '../_graphql/categories'
 import { ORDER } from '../_graphql/orders'
 import { PAGE } from '../_graphql/pages'
 import { PRODUCT } from '../_graphql/products'
